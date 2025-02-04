4945db43657f9c06fc2d77c5cb8d9ea83cb0e9df
Khomenko Nadiia <n.homenko395@gmail.com>
2025-02-04 15:30:40 +0200

    завдання 1

---
+++ ConsoleApp/task 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+                Console.Write("Введіть перше число: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Введіть друге число: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                int sum = num1 + num2;
+
+                Console.WriteLine($"{num1} + {num2} = {sum}");
+            }
+        }
+    }
+
+}
